36e60df80c19b9ee1572771317a4ce5f6846a55b
Mathias Henze <mhenze@jeto.de>
2020-04-26 16:26:53 +0200

    added maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,24 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.71"
+    id("com.vanniktech.maven.publish") version "0.11.1"
+}
+
+buildscript {
+    // dokka requires a repository from which to download dokka-fatjar on demand
+    configure(listOf(repositories, project.repositories)) {
+        jcenter()
+        maven { url = uri("https://dl.bintray.com/kotlin/kotlin-dev") }
+    }
 }
 
 val awssdkVersion : String by ext
 val awssdkService : String by ext
+val GROUP : String by ext
+val VERSION_NAME : String by ext
 
-group = "net.highteq.cylab"
-version = "${awssdkVersion}-ALPHA"
+group = GROUP
+version = VERSION_NAME
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 tasks.withType<KotlinCompile> {
